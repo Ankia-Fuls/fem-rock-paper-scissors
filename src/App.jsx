9835b2b7ff5cd9f222faced49c48a0a97a4f62a4
Ankia-Fuls <ankiafuls01@gmail.com>
2026-06-23 16:24:26 +0200

    Extracted Starter Pack

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './css/styles.scss'
+
+function App() {
+
+
+  return (
+    <>
+      <main>
+        Score
+        Rules
+
+        You Picked
+        The House Picked
+
+        You Win
+        You Lose
+
+        Play Again
+
+      </main>
+
+      <footer class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+        Coded by <a href="#">Your Name Here</a>.
+      </footer>
+
+    </>
+  )
+}
+
+export default App
