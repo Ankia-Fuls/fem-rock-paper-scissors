f2613beb2db0e9f65d4a0bb445dd2161559149d5
Ankia-Fuls <ankiafuls01@gmail.com>
2026-06-23 17:23:47 +0200

    Basics of modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './css/styles.scss'
 
 function App() {
 
+  const [rules, setRules] = useState({
+    "scissors": { "win": ["paper", "lizard"], "lose": ["rock", "spock"] },
+    "paper": { "win": ["rock", "spock"], "lose": ["scissors", "lizard"] },
+    "rock": { "win": ["scissors", "lizard"], "lose": ["paper", "spock"] },
+    "lizard": { "win": ["paper", "spock"], "lose": ["rock", "scissors"] },
+    "spock": { "win": ["scissors", "rock"], "lose": ["lizard", "paper"] }
+  });
+
+  const dialogRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const dialog = dialogRef.current;
+
+    if (isOpen) {
+      dialog.showModal();
+    } else {
+      dialog.close();
+    }
+
+    return () => {
+      dialog.close();
+    };
+  }, [isOpen]);
+
+  //how to test if win
+  // console.log(rules["scissors"]["win"].includes("paper"))
+
+  // make area outside clickable
+  // const closeModalClick = (e) => {
+  //   const dialog = dialogRef.current;
+  //   const rect = dialog.getBoundingClientRect();
+  //   if (
+  //     e.clientX < rect.left ||
+  //     e.clientX > rect.right ||
+  //     e.clientY < rect.top ||
+  //     e.clientY > rect.bottom
+  //   ) {
+  //     dialog.close();
+  //   }
+  // }
 
   return (
     <>
@@ -17,12 +58,17 @@ function App() {
         You Lose
 
         Play Again
+        <button onClick={() => setIsOpen(true)}>Rules</button>
+
+        <dialog ref={dialogRef}>
+          <button onClick={() => setIsOpen(false)}>Close</button>
+        </dialog>
 
       </main>
 
-      <footer class="attribution">
+      <footer className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
-        Coded by <a href="#">Your Name Here</a>.
+        Coded by <a href="https://github.com/Ankia-Fuls">Ankia Fuls</a>.
       </footer>
 
     </>
