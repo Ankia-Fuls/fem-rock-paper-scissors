cc158da374d8c846511ee8119f0c1c97327cdd66
Ankia-Fuls <ankiafuls01@gmail.com>
2026-06-27 13:33:41 +0200

    Button Positioning Mobile

--- src/App.jsx
+++ src/App.jsx
@@ -157,28 +157,28 @@ function App() {
             </button>
 
             {/* PAPER */}
-            <button className='choice__btn choice__btn--blue' onClick={() => choose("paper")} aria-label='Choose paper' id="paper">
+            <button className='choice__btn' onClick={() => choose("paper")} aria-label='Choose paper' id="paper">
               <div className='choice__btn--wrapper'>
                 <img src="./src/assets/images/icon-paper.svg" alt="" />
               </div>
             </button>
 
             {/* ROCK */}
-            <button className='choice__btn choice__btn--red' onClick={() => choose("rock")} aria-label='Choose rock' id="rock">
+            <button className='choice__btn' onClick={() => choose("rock")} aria-label='Choose rock' id="rock">
               <div className='choice__btn--wrapper'>
                 <img src="./src/assets/images/icon-rock.svg" alt="" />
               </div>
             </button>
 
             {/* LIZARD */}
-            <button className='choice__btn choice__btn--purple' onClick={() => choose("lizard")} aria-label='Choose lizard' id="lizard">
+            <button className='choice__btn' onClick={() => choose("lizard")} aria-label='Choose lizard' id="lizard">
               <div className='choice__btn--wrapper'>
                 <img src="./src/assets/images/icon-lizard.svg" alt="" />
               </div>
             </button>
 
             {/* SPOCK */}
-            <button className='choice__btn choice__btn--cyan' onClick={() => choose("spock")} aria-label='Choose spock' id="spock">
+            <button className='choice__btn' onClick={() => choose("spock")} aria-label='Choose spock' id="spock">
               <div className='choice__btn--wrapper'>
                 <img src="./src/assets/images/icon-spock.svg" alt="" />
               </div>
